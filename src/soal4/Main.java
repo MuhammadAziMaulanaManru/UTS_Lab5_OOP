693d5a425a90b8cbcb314b388f6190659c7aef7b
Muhammad Azi <muhammadazi150924@gmail.com>
2024-05-22 12:43:30 +0700

    menyeleaikan soal 3

--- src/soal4/Main.java
+++ src/soal4/Main.java
@@ -3,5 +3,16 @@ package soal4;
 public class Main {
     public static void main(String[] args) {
 
+        // Contoh penggunaan
+        MeltingIce ice1 = new MeltingIce(2); // Massa 2 kg, kalor lebur default
+        System.out.println("Energi yang diperlukan untuk melelehkan 2 kg es: " + ice1.calculateEnergy() + " Joule");
+
+        MeltingIce ice2 = new MeltingIce(3, 340000); // Massa 3 kg, kalor lebur 340000 J/kg
+        System.out.println("Energi yang diperlukan untuk melelehkan 3 kg es dengan kalor lebur 340000 J/kg: " + ice2.calculateEnergy() + " Joule");
+
+        // Menggunakan method overloading
+        System.out.println("Energi yang diperlukan untuk melelehkan 4 kg es: " + ice1.calculateEnergy(4) + " Joule");
+        System.out.println("Energi yang diperlukan untuk melelehkan 5 kg es dengan kalor lebur 350000 J/kg: " + ice1.calculateEnergy(5, 350000) + " Joule");
+    }
 }
 
--- src/soal4/MeltingIce.java
+++ src/soal4/MeltingIce.java
@@ -1,3 +1,26 @@
+/*
+ *
+ *      Q=m⋅Lf
+ *      dimana :
+ *      Q adalah jumlah kalor yang diperlukan (Joule, J).
+ *      m adalah massa es yang akan dilelehkan (kilogram, kg).
+ *      Lf adalah kalor lebur es (kalor laten peleburan), yang untuk air adalah 334,000 J/kg
+ *
+ *      1. Cara kerja:
+ *      Massa Es:
+ *      Tentukan massa es yang ingin dilelehkan. Misalnya,𝑚 = 0.5
+ *
+ *      2. Kalor lebur es
+ *      Nilai kalor lebur untuk es adalah 𝐿𝑓 = 334,000 J/kg
+ *
+ *      3. Menghitung Kalor yang Diperlukan:
+ *      Gunakan rumus 𝑄 = 𝑚⋅𝐿𝑓 Q=m⋅Lf.
+ *
+ *      Masukkan nilai massa es dan kalor lebur:
+ *      Q=0.5kg×334,000J/kg=167,000J
+ *
+ */
+
 package soal4;
 
 public class MeltingIce {
